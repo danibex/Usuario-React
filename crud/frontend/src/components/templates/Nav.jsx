f9423016351018ce6eb591e202ec4f21288dd248
danibex <danielivam96@gmail.com>
2023-04-10 20:23:54 -0300

    .

--- crud/frontend/src/components/templates/Nav.jsx
+++ crud/frontend/src/components/templates/Nav.jsx
@@ -6,7 +6,6 @@ import NavItem from './NavItem'
 export default props => (
     <aside className='menu-area'>
         <nav className='menu'>
-        {/* Refatorar!!! */}
             <NavItem href="/" icon="home" name="Início"/>
             <NavItem href="/users" icon="users" name="Usuários"/>
         </nav>
